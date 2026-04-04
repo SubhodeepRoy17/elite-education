5d0c0383cadab64e42434ba846dac343eed3c2fa
Subhodeep Roy <144267929+SubhodeepRoy17@users.noreply.github.com>
2026-04-04 15:15:55 +0000

    next version updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    localPatterns: [
+      {
+        pathname: '/profile.png',
+        search: '?'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
